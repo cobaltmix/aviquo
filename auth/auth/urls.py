a59f80946105fb284556949f629a4d6c1cac8458
Srivatsav21 <srivatsavkannan@gmail.com>
2023-09-20 15:32:32 +0530

    Added ability to modify user once created, can add email, first name, and last name.

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -3,7 +3,7 @@ URL configuration for auth project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
+Examples:wdwf
 Function viewsee
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
