47d1d8e5a4eb8327fa32ea4d45f045fa58e307d8
Srivatsav21 <srivatsavkannan@gmail.com>
2023-09-20 11:31:28 +0530

    Can add ECs from admin panel

--- auth/users/admin.py
+++ auth/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ExtracurricularReference
+
+@admin.register(ExtracurricularReference)
+class ExtracurricularReferenceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'website', 'field', 'type', 'mode', 'season', 'selectivity', 'cost']
