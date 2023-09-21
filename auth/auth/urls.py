2167ec1aff6f8a342cf443b25ed1969f0ad4a2e3
Srivatsav21 <srivatsavkannan@gmail.com>
2023-09-21 10:04:04 +0530

    Fixed a bug where website crashes after login. accounts/profile now displays same info as home/profile. Might have to clean up the url configuration later.

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+import users.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
     path('home/', include('users.urls')),
     path('api/', include('api.urls')),
 ]
