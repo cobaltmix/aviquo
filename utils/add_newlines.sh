2ff66ee6e02ac88ec47466a11c01fac961b5860f
Leo-MathGuy <leo.mathguy@gmail.com>
2023-09-24 03:06:49 +0000

    Added newlines fr

--- utils/add_newlines.sh
+++ utils/add_newlines.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-find ../auth -type f -exec sh -c 'if [ "$(tail -c 1 "$0" 2>/dev/null)" != "" ]; then echo "" >> "$0"; fi' {} \;
+find ../auth -type f -exec sh -c 'if [ "$(tail -c 1 "$0" 2>/dev/null)" != "\n" ]; then echo "" >> "$0"; fi' {} \;
