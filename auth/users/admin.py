fc563403d5afa222e515b7f202c32f2ca526a524
Srivatsav21 <srivatsavkannan@gmail.com>
2023-09-26 19:11:58 +0530

    Registered tag with Admin

--- auth/users/admin.py
+++ auth/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Opportunity, Forum
+from .models import Opportunity, Forum, Tag
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -8,3 +8,4 @@ User = get_user_model()
 # Register your models with the custom admin classes
 admin.site.register(Opportunity)
 admin.site.register(Forum)
+admin.site.register(Tag)
