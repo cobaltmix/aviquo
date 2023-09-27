eb99521d79c89d9359d6d10b76d3b58d9fd222cf
Lol Lol <theperkoinc@gmail.com>
2023-09-27 19:40:14 +0300

    new model for waitlist (endpoint: api/waitlist)

--- auth/users/admin.py
+++ auth/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Opportunity, Forum, Tag
+from .models import Opportunity, Forum, Tag, Waitlist
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -9,3 +9,4 @@ User = get_user_model()
 admin.site.register(Opportunity)
 admin.site.register(Forum)
 admin.site.register(Tag)
+admin.site.register(Waitlist)
