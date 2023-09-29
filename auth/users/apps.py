64fb1ac0756fe2200ca3bb6556b84969f479f155
Srivatsav21 <srivatsavkannan@gmail.com>
2023-09-29 12:03:00 +0530

    Renamed auth folder to backend

--- auth/users/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
