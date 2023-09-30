121487189c7bd489c445632f1030c51a62675bcb
Leo-MathGuy <leonidiudakov@gmail.com>
2023-09-30 15:21:21 -0700

    Reworked ladning page, added desktop and made more variablely

--- utils/add_newlines.sh
+++ utils/add_newlines.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-for file in $(find . -type f \( -name "*.md" -o -name "*.html" -o -name "*.py" \)); do
+for file in $(find . -type f \( -name "*.md" -o -name "*.html" -o -name "*.py" -o -name "*.css" -o -name "*.js" \)); do
     echo -n -e "\033[1K"
     echo -n -e "\r$file"
     sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$file"
