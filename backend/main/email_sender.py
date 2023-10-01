116e7075703306d3a62865e8fe301e31e52c29cf
appuser at t2 <level@gotovo.com>
2023-10-01 16:20:50 -0500

    rename auth->core, users->main, update User model, etc.

--- backend/main/email_sender.py
+++ backend/main/email_sender.py
@@ -1,22 +1,19 @@
 import smtplib
 import ssl
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from django.contrib.auth.views import PasswordResetView
 from django.contrib.auth.forms import PasswordResetForm
-
+from django.contrib.auth.views import PasswordResetView
 from django.template.loader import render_to_string
-from django.contrib.auth import get_user_model
-User = get_user_model()
+
 
 class Email:
     def __init__(self, receiver):
         self.ctx = ssl.create_default_context()
-        self.password = 'PASS HERE'    # Your app password goes here
-        self.sender = 'pythondiscordbot88@gmail.com'    # Your e-mail address
-        self.receiver = receiver # Recipient's address
+        self.password = "PASS HERE"  # Your app password goes here
+        self.sender = "pythondiscordbot88@gmail.com"  # Your e-mail address
+        self.receiver = receiver  # Recipient's address
 
     def createHeaders(self, subject):
         self.message = MIMEMultipart("alternative")
@@ -24,7 +21,7 @@ class Email:
         self.message["From"] = "Aviquo"
         self.message["To"] = self.receiver
 
-    #order is server invite link, dashboard link (wrapped of course)
+    # order is server invite link, dashboard link (wrapped of course)
     def createBody(self, html):
         self.html = html
 
@@ -41,10 +38,11 @@ class Email:
 class EmailPasswordResetForm(PasswordResetForm):
     def send_mail(_, subject_template_name, email_template_name, context, from_email, to_email, *args, **kwargs):
         e = Email(to_email)
-        e.createHeaders('Password Reset Link')
+        e.createHeaders("Password Reset Link")
         e.createBody(render_to_string(email_template_name, context))
         e.sendMessage()
 
+
 class CustomPasswordResetView(PasswordResetView):
-    email_template_name = 'accounts/custom_reset_email.html'
+    email_template_name = "accounts/custom_reset_email.html"
     form_class = EmailPasswordResetForm
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,42 +1,40 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
-from .models import Opportunity, Forum, Waitlist
-from rest_framework import generics
-from .serializers import OpportunitySerializer, ForumSerializer, WaitlistSerializer
 from django import forms
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from rest_framework import generics
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from .models import Forum, Opportunity, Waitlist, User
+from .serializers import ForumSerializer, OpportunitySerializer, WaitlistSerializer
+
 
 # Create your views here.
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ["username", "email", "first_name", "last_name"]
+
 
 def home(request):
-    return render(request, 'users/home.html', {})
+    return render(request, "users/home.html", {})
+
 
 @login_required
 def profile(request):
     user = request.user
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = EditProfileForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('profile')
+            return redirect("profile")
 
     else:
         form = EditProfileForm(instance=user)
 
-    return render(request, 'users/profile.html', {'user': user, 'form': form})
-
+    return render(request, "users/profile.html", {"user": user, "form": form})
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -44,21 +42,25 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ("email",)
+
 
 class SignUp(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+
 class ForumView(generics.CreateAPIView):
     queryset = Forum.objects.all()
     serializer_class = ForumSerializer
 
+
 class OpportunityView(generics.CreateAPIView):
     queryset = Opportunity.objects.all
-    serializer_class  = OpportunitySerializer
+    serializer_class = OpportunitySerializer
+
 
 class WaitlistView(generics.CreateAPIView):
     queryset = Waitlist.objects.all
-    serializer_class  = WaitlistSerializer
+    serializer_class = WaitlistSerializer
