6b8d9c1c0d94b50114b7748fb315de2ed891e420
appuser at t2 <level@gotovo.com>
2023-10-02 18:39:18 -0500

    asgi config - not tested

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -3,7 +3,31 @@ ASGI config for the project.
 """
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
+
+from chat.consumers import TextRoomConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = get_asgi_application()
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+
+websocket_urlpatterns = [
+    re_path(r"^ws/(?P<room_name>[^/]+)/$", TextRoomConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter(
+    {
+        # Django's ASGI application to handle traditional HTTP requests
+        "http": django_asgi_app,
+        # WebSocket chat handler
+        "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+    }
+)
