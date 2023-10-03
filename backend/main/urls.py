690213fb421aa9c648dae1dbf7ca6c0d9cf770ab
appuser at t2 <level@gotovo.com>
2023-10-02 19:27:33 -0500

    Make migrations, fix typo

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from .email_sender import CustomPasswordResetView
 from .views import ForumView, OpportunityView, SignUp, WaitlistView, home, profile
 
+
 urlpatterns = [
     path("", home, name="home"),
     path("signup/", SignUp.as_view(), name="signup"),
-    path("opportunity/", OpportunityView.as_view(), name="opportunity"),
-    path("waitlist /", WaitlistView.as_view(), name="waitlist"),
+    path("opportunity/", OpportunityView, name="opportunity"),
+    path("waitlist/", WaitlistView.as_view(), name="waitlist"),
     path("forum/", ForumView.as_view(), name="forum"),
     path("profile/", profile, name="profile"),
     path("password_reset/", CustomPasswordResetView.as_view(), name="password_reset"),
