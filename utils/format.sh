cdc537a8f9f53b730f53bb50139119ce80f9f7eb
Leo-MathGuy <leo.mathguy@gmail.com>
2023-10-03 19:12:46 +0000

    updated format script

--- utils/format.sh
+++ utils/format.sh
@@ -2,5 +2,8 @@
 
 # Formatting, idk if you wanna github-actionify it or something
 
+read -p "Relative directory to format from: " path_from
+cd $path_from
+
 echo "NEWLINES:"
 utils/add_newlines.sh
